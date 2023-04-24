5222f27aa58fc6f07191c182a1712f0a2cefef50
sunman24 <andrey_nuzhnov.01@mail.ru>
2023-04-24 20:31:06 +0700

    Updated the gradle configurations in the database and models modules

--- shared/database/build.gradle.kts
+++ shared/database/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        minSdk = 16
+        minSdk = 14
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
