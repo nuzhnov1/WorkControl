82781d8120982a1533418f580144a10f2f260273
sunman24 <andrey_nuzhnov.01@mail.ru>
2023-04-25 14:59:55 +0700

    Removed the visitservice module

--- shared/visitservice/src/main/kotlin/com/nuzhnov/workcontrol/shared/visitservice/Constants.kt
+++
@@ -1,4 +0,0 @@
-package com.nuzhnov.workcontrol.shared.visitservice
-
-
-const val VISITORS_DATABASE_NAME = "com-nuzhnov-workcontrol-shared-visitservice-visitors"
--- shared/visitservice/src/main/kotlin/com/nuzhnov/workcontrol/shared/visitservice/domen/service/visitor/VisitorServiceCommand.kt
+++
@@ -1,8 +0,0 @@
-package com.nuzhnov.workcontrol.shared.visitservice.domen.service.visitor
-
-import java.io.Serializable
-
-internal sealed interface VisitorServiceCommand : Serializable {
-    object Discover : VisitorServiceCommand
-    data class Connect(val serviceName: String) : VisitorServiceCommand
-}
