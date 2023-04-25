044d27178b328a4422d10fab5453e19192c0bf43
sunman24 <andrey_nuzhnov.01@mail.ru>
2023-04-25 14:59:31 +0700

    Updated the database and the resources modules

--- shared/resources/src/main/kotlin/com/nuzhnov/workcontrol/shared/resources/Extensions.kt
+++ shared/resources/src/main/kotlin/com/nuzhnov/workcontrol/shared/resources/Extensions.kt
@@ -4,11 +4,6 @@ import com.nuzhnov.workcontrol.shared.models.Lesson
 import android.content.Context
 
 
-fun Lesson.Type.toResourceString(context: Context): String = when (this) {
-    Lesson.Type.LECTURE -> context.getString(R.string.lecture_lesson_type)
-    Lesson.Type.PRACTICE -> context.getString(R.string.practice_lesson_type)
-}
-
 fun Lesson.VisitType.toResourceString(context: Context): String = when (this) {
     Lesson.VisitType.INTRAMURAL -> context.getString(R.string.intramural_visit_type)
     Lesson.VisitType.REMOTE -> context.getString(R.string.remote_visit_type)
