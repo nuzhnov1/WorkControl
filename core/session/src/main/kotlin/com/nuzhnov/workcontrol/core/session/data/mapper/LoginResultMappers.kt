475bb187ed77e02ee41b2362ec97e42925ff68bb
sunman24 <andrey_nuzhnov.01@mail.ru>
2023-05-12 23:48:58 +0700

    Updated ':core:session' module

--- core/session/src/main/kotlin/com/nuzhnov/workcontrol/core/session/data/mapper/LoginResultMappers.kt
+++ core/session/src/main/kotlin/com/nuzhnov/workcontrol/core/session/data/mapper/LoginResultMappers.kt
@@ -1,15 +1,9 @@
 package com.nuzhnov.workcontrol.core.session.data.mapper
 
 import com.nuzhnov.workcontrol.core.session.domen.model.LoginResult
-import com.nuzhnov.workcontrol.core.session.domen.model.UserData
 import com.nuzhnov.workcontrol.core.api.constant.LoginErrorMessages
 import com.nuzhnov.workcontrol.core.api.util.Response
 
-
-internal fun Response<UserData>.toLoginResult(): LoginResult = when (this) {
-    is Response.Success<UserData> -> LoginResult.Success(userData = value)
-    is Response.Failure -> this.toFailureLoginResult()
-}
 
 internal fun Response.Failure.toFailureLoginResult(): LoginResult.Failure = when (this) {
     is Response.Failure.HttpClientError.BadRequest -> this.toFailureLoginResult()
@@ -20,7 +14,7 @@ internal fun Response.Failure.toFailureLoginResult(): LoginResult.Failure = when
 }
 
 private fun Response.Failure.HttpClientError.BadRequest.toFailureLoginResult(): LoginResult.Failure =
-    when (message) {
+    when (cause.message()) {
         LoginErrorMessages.WRONG_LOGIN -> LoginResult.Failure.WrongLogin
         LoginErrorMessages.WRONG_PASSWORD -> LoginResult.Failure.WrongPassword
         else -> LoginResult.Failure.UnknownError
